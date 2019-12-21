d00dd6ca7484d4f0ba52e4b82fcfdba2bd3efb06
FlorinChertes <FlorinChertes@computer.org>
2019-12-21 21:15:04 +0100

    add Concepts
    Some Concepts examles added

--- lib/inc/type_func.h
+++ lib/inc/type_func.h
@@ -125,6 +125,7 @@ namespace func_lib_ex
 
 namespace func_lib
 {
+
 	template<typename R, typename T>
 	bool in(R const& range, T const& value)
 	{
@@ -138,13 +139,69 @@ namespace func_lib
 		return false;
 	}
 
-	template<typename R, typename T>
-	bool in_c(R const& range, T const& value)
+	//-----------------------------------------------------------------------------
+	//-----------------------------------------------------------------------------
+	template <typename T>
+	concept Equality_comparable = requires (T a, T b)
 	{
-		for (auto const& x : range)
-		{
-			if (x == value)
-			{
+		{a == b} -> bool;
+		{a != b} -> bool;
+	};
+
+	//-----------------------------------------------------------------------------
+	//-----------------------------------------------------------------------------
+	template <typename T, typename U>
+	concept Same = std::is_same<T, U>::value;
+
+	//-----------------------------------------------------------------------------
+	//-----------------------------------------------------------------------------
+	// The value_type of a class is a member type
+	template<typename T>
+	struct value_type;
+
+	template<typename T>
+	using value_type_t = typename value_type<T>::type;
+
+
+	// The value_type of a class is a member type
+	template<typename T>
+	requires requires {typename T::value_type; }
+	struct value_type<T>
+	{
+		using type = typename T::value_type;
+	};
+
+	// The value_type of a pointer is the type of element pointed to.
+	template<typename T>
+	struct value_type<T*>
+	{
+		using type = T;
+	};
+
+	// The value_type of an array is its element type.
+	template<typename T, int N>
+	struct value_type<T[N]>
+	{
+		using type = T;
+	};
+
+	//-----------------------------------------------------------------------------
+	//-----------------------------------------------------------------------------
+	template <typename R>
+	concept Range = requires (R range)
+	{
+		range.begin();
+		range.end();
+	};
+
+	//-----------------------------------------------------------------------------
+	//-----------------------------------------------------------------------------
+	template<Range R, Equality_comparable T>
+	requires Same<value_type_t<R>, T>
+		bool in_c(R const& range, T const& value)
+	{
+		for (const auto& x : range) {
+			if (x == value) {
 				return true;
 			}
 		}
