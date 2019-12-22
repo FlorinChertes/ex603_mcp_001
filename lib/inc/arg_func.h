356ec4327e2b52e4495683da53303a49125be8f3
FlorinChertes <FlorinChertes@computer.org>
2019-12-22 20:19:56 +0100

    array size
    use template to calculate an array size

---
+++ lib/inc/arg_func.h
@@ -0,0 +1,8 @@
+
+#include <cstddef>
+
+template<typename T, std::size_t N>
+constexpr std::size_t arraySize(T(&)[N]) noexcept
+{
+	return N;
+}
--- test/src/test_func_arg.cpp
+++ test/src/test_func_arg.cpp
@@ -1,24 +1,32 @@
 #include <test_func_arg.h>
 
-#include <functional>
-#include <cmath>
+#include <arg_func.h>
+
 #include <array>
-#include <list>
-#include <queue>
-#include <map>
-#include <vector>
 #include <iostream>
-
-#include <string>
-#include <algorithm>
-
-#include <stdexcept>
 #include <cassert>
-
 
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
 void test_func_arg_001()
 {
 	std::cout << "*** test func arg 001 ***" << std::endl;
+
+	int keyVals[] = {1,2,3,4,5,6};
+	int mappedVals[arraySize(keyVals)];
+
+	assert(arraySize(keyVals) == 6);
+
+	std::copy(keyVals, keyVals + arraySize(keyVals), 	//source
+		std::ostream_iterator<int>(std::cout, ", "));	//destination
+	std::cout << std::endl;
+
+	std::copy(keyVals, keyVals + arraySize(keyVals), 	//source
+		mappedVals);									//destination
+
+	std::copy(mappedVals, mappedVals + arraySize(mappedVals), 	//source
+		std::ostream_iterator<int>(std::cout, ", "));			//destination
+	std::cout << std::endl;
+
+	std::cout << "size of array is: " << arraySize(keyVals) << std::endl;
 }
