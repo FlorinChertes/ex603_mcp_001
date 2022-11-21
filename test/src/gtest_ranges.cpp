15ae21cb05fbb7516da2b5d89e981995ddab3fbe
Florin Chertes <FlorinChertes@computer.org>
2022-11-21 20:27:37 +0100

    ranges change

--- test/src/gtest_ranges.cpp
+++ test/src/gtest_ranges.cpp
@@ -287,11 +287,10 @@ TEST(RangesTest, test_find_first_of_ranges)
 		std::cout << "std::ranges::find_first_of: " << it->name_
 			<< " at: " << pos << '\n';
 
-		const auto rest_of_prods = prods | std::views::drop(pos + 1);
-		auto it2 = std::ranges::find_first_of(rest_of_prods, arrInvalids,
+		auto it2 = std::ranges::find_first_of(prods | std::views::drop(pos + 1), arrInvalids,
 			std::ranges::equal_to{}, &Product::name_);
-		EXPECT_NE(it2, rest_of_prods.end());
-		if (it2 != end(rest_of_prods))
+		EXPECT_NE(it2, prods.end());
+		if (it2 != end(prods))
 			std::cout << "std::ranges::find_first_of: " << it2->name_
 			<< " at: " << std::distance(begin(prods), it2) << '\n';
 	}
