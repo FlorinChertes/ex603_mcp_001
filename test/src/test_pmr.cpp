59272554db00e0347644d60453522a3a3c1e2077
FlorinChertes <FlorinChertes@computer.org>
2023-07-25 19:56:19 +0200

    pmr second example

--- test/src/test_pmr.cpp
+++ test/src/test_pmr.cpp
@@ -136,3 +136,36 @@ void test_pmr_067()
 
     std::cout << "\n*** test 067 end ***" << std::endl;
 }
+
+void test_pmr_068()
+{
+    std::cout << "sizeof(std::string): " << sizeof(std::string) << '\n';
+    std::cout << "sizeof(std::pmr::string): " << sizeof(std::pmr::string) << '\n';
+
+    char buffer[256] = {}; // a small buffer on the stack
+    std::fill_n(std::begin(buffer), std::size(buffer) - 1, '_');
+
+    const auto BufferPrinter = [](std::string_view buf, std::string_view title) {
+        std::cout << title << ":\n";
+        for (auto& ch : buf) {
+            std::cout << (ch >= ' ' ? ch : '#');
+        }
+        std::cout << '\n';
+    };
+
+    BufferPrinter(buffer, "zeroed buffer");
+
+    std::pmr::monotonic_buffer_resource pool{std::data(buffer), std::size(buffer)};
+    std::pmr::vector<std::pmr::string> vec{ &pool };
+    vec.reserve(5);
+
+    vec.push_back("Hello World");
+    vec.push_back("One Two Three");
+    BufferPrinter(std::string_view(buffer, std::size(buffer)), "after two short strings");
+
+    vec.emplace_back("This is a longer string");
+    BufferPrinter(std::string_view(buffer, std::size(buffer)), "after longer string strings");
+
+    vec.push_back("Four Five Six");
+    BufferPrinter(std::string_view(buffer, std::size(buffer)), "after the last string");
+}
