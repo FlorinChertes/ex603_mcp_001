f574ce52519f8f44bcd6a4e9f3468ef9171471ae
unknown <Florin Chertes>
2025-10-12 21:19:22 +0200

    OpenEDG 003

--- main/main_05.cpp
+++ main/main_05.cpp
@@ -8,6 +8,8 @@ int main ([[maybe_unused]] int argc, [[maybe_unused]] char *argv[])
 	std::cout << "test main 05" << std::endl;
 
 	test_openEDG_010();
+	test_openEDG_011();
+	test_openEDG_012();
 
 	return 0;
 }
--- test/inc/open_edg/test_array.h
+++ test/inc/open_edg/test_array.h
@@ -1,1 +1,3 @@
 void test_openEDG_010();
+void test_openEDG_011();
+void test_openEDG_012();
